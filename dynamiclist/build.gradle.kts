fa8547643fc47e782acc5a4a056b4de238b63ed8
Akhil Mohan R <akhilmohanr.0@gmail.com>
2024-12-21 03:17:52 +0530

    initial commit

--- dynamiclist/build.gradle.kts
+++ dynamiclist/build.gradle.kts
@@ -1,6 +1,8 @@
+import org.gradle.kotlin.dsl.from
+
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.library") version "8.7.1"
+    id("org.jetbrains.kotlin.android") version "1.9.24"
     `maven-publish`
 }
 
@@ -8,13 +10,10 @@ android {
     namespace = "com.blackdot.dynamiclist"
     compileSdk = 34
 
-
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
-
     }
 
     buildTypes {
@@ -35,14 +34,14 @@ android {
     }
 }
 
+//noinspection UseTomlInstead
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
 
 afterEvaluate {
